adb8210aa9a4059a02428f76d033b10cacd0a5c3
Renan <renanvini1st@gmail.com>
2025-09-25 15:27:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Dados básicos do personagem
+// Dados basicos
 const frozone = {
     nome: "Lucius Best",
     codinome: "Frozone",
@@ -17,22 +17,18 @@ const frozone = {
     ]
 };
 
-// Rota principal
 app.get('/', (req, res) => {
     res.send("Bem-vindo à API do Frozone!");
 });
 
-// Retorna dados do Frozone
 app.get('/frozone', (req, res) => {
     res.json(frozone);
 });
 
-// Retorna apenas habilidades
 app.get('/frozone/habilidades', (req, res) => {
     res.json(frozone.habilidades);
 });
 
-// Retorna frases famosas
 app.get('/frozone/frases', (req, res) => {
     res.json(frozone.frasesFamosas);
 });
